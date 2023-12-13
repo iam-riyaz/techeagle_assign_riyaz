2acb6d0b6b06e98594913f682f805dc95522314b
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2023-12-13 23:16:34 +0530

    hashing added

--- backend/src/controllers/user.contoller.js
+++ backend/src/controllers/user.contoller.js
@@ -1,12 +1,18 @@
 
 
 import * as userService from "../services/user.services.js"
+import bcrypt from "bcrypt"
 
 export const signUp=async(req,res)=>{
     try{
      const {name,email,password,address}=req.body
+    
 
-     const user= await userService.signUp({name,email,password,address})
+     const salt =  bcrypt.genSaltSync(10);
+     const hashedPassword =  bcrypt.hashSync(password, salt);
+     
+
+     const user= await userService.signUp({name,email,password:hashedPassword,address})
      res.status(201).send({status:"success",user})
         
     }
